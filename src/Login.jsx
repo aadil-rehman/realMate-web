090401b467aa2e62dbb9ffde621138616253d444
Moh Aadil <aadilrehman.development@gmail.com>
2025-03-27 03:33:22 +0530

    add: login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-	return <div>login</div>;
+	const [emailId, setEmailId] = useState("aadil@gmail.com");
+	const [password, setPassword] = useState("Aadil@123");
+	const handleLogin = async () => {
+		try {
+			const res = await axios.post(
+				"http://localhost:3000/login",
+				{
+					emailId,
+					password,
+				},
+				{ withCredentials: true }
+			);
+			console.log(res);
+		} catch (err) {
+			console.error("ERROR: " + err);
+		}
+	};
+
+	return (
+		<div className="flex justify-center my-20">
+			<div className="card card-border bg-base-300 w-96 ">
+				<div className="card-body">
+					<h2 className="card-title flex justify-center">Login</h2>
+					<input
+						type="text"
+						value={emailId}
+						onChange={(e) => setEmailId(e.target.value)}
+						placeholder="Email"
+						className="input"
+					/>
+					<input
+						type="text"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+						className="input"
+					/>
+					<div className="card-actions justify-center">
+						<button className="btn btn-primary" onClick={handleLogin}>
+							Login
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
