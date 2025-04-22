77a3ac9296ef1373d44ee6ca905c66a203834b09
Moh Aadil <aadilrehman.development@gmail.com>
2025-04-23 02:06:33 +0530

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,12 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
-		<footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-4 py-2 fixed bottom-0 w-full">
-			<aside>
-				<p>
-					Copyright © {new Date().getFullYear()} - All rights reserved by
-					RealMate Pvt. Ltd.
-				</p>
-			</aside>
-			<div className="flex gap-4">
+		<footer className="flex justify-between items-center bg-base-300 text-base-content px-4 py-2 fixed bottom-0 w-full text-sm">
+			<p className="whitespace-nowrap">
+				Copyright © {new Date().getFullYear()} - All rights reserved by RealMate
+				Pvt. Ltd.
+			</p>
+			<div className="flex gap-5">
 				<Link to="/termsofservice" className="link link-hover text-xs">
 					Terms of Service
 				</Link>
